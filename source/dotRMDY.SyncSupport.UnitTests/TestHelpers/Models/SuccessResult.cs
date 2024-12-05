eb36bd12b6c4456ac2b7d37c5e28a7d75ac96354
Maxim Storms <maxim@maximst.be>
2024-12-05 12:17:51 +0100

    Add unit tests

---
+++ source/dotRMDY.SyncSupport.UnitTests/TestHelpers/Models/SuccessResult.cs
@@ -0,0 +1,8 @@
+namespace dotRMDY.SyncSupport.UnitTests.TestHelpers.Models;
+
+public class SuccessResult
+{
+	public string? A { get; set; }
+	public bool B { get; set; }
+	public int C { get; set; }
+}
